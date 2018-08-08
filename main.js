cd13cf758ae2565d2c8f87cd67ef100c77f473c2
josephrfarmer <josephr.farmer24@gmail.com>
2018-08-08 15:37:12 -0400

    html and css is done, started JS
    started event listener for clicking buttons

---
+++ main.js
@@ -0,0 +1,88 @@
+var one = document.getElementById('one');
+var two = document.getElementById('two');
+var three = document.getElementById('three');
+var four = document.getElementById('four');
+var five = document.getElementById('five');
+var six = document.getElementById('six');
+var seven = document.getElementById('seven');
+var eight = document.getElementById('eight');
+var nine = document.getElementById('nine');
+var zero = document.getElementById('zero');
+var divide = document.getElementById('divide');
+var times = document.getElementById('times');
+var minus = document.getElementById('minus');
+var decimal = document.getElementById('decimal');
+var plus = document.getElementById('plus');
+var clear = document.getElementById('clear');
+var equals = document.getElementById('equals');
+
+// Event Listeners
+
+one.addEventListener('click', function(e){
+    console.log("1")
+})
+
+two.addEventListener('click', function(e){
+    console.log("2")
+}) 
+
+three.addEventListener('click', function(e){
+    console.log("3")
+}) 
+
+four.addEventListener('click', function(e){
+    console.log("4")
+})
+
+five.addEventListener('click', function(e){
+    console.log("5")
+})
+
+six.addEventListener('click', function(e){
+    console.log("6")
+})
+
+seven.addEventListener('click', function(e){
+    console.log("7")
+})
+
+eight.addEventListener('click', function(e){
+    console.log("8")
+})
+
+nine.addEventListener('click', function(e){
+    console.log("9")
+})
+
+zero.addEventListener('click', function(e){
+    console.log("0")
+})
+
+divide.addEventListener('click', function(e){
+    console.log("/")
+})
+
+times.addEventListener('click', function(e){
+    console.log("x")
+})
+
+minus.addEventListener('click', function(e){
+    console.log("-")
+})
+
+decimal.addEventListener('click', function(e){
+    console.log(".")
+})
+
+plus.addEventListener('click', function(e){
+    console.log("+")
+})
+
+clear.addEventListener('click', function(e){
+    console.log("clear")
+})
+
+equals.addEventListener('click', function(e){
+    console.log("=")
+})
+
